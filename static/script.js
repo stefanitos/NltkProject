1118ffe09f8ef6a3f0a3f571922bd32778daf688
nefast <9hprjt9gd@mozmail.com>
2024-04-17 20:24:16 +0200

    changes

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ window.onload = function () {
     pointer: {
       length: 0.6,
       strokeWidth: 0.039,
-      color: "#0000000",
+      color: "#000000", // corrected color value
     },
     limitMax: false,
     limitMin: false,
@@ -17,67 +17,72 @@ window.onload = function () {
     highDpiSupport: true,
   };
 
-  var article_list = {};
+  let article_list = [];
 
   populateArticleList();
 
   document.getElementById("btn-submit").addEventListener("click", function () {
+    displayResult("val-url", "/analyze");
+    populateArticleList();
+  });
+
+  document
+    .getElementById("article-list")
+    .addEventListener("change", function () {
+      displayResult("article-list");
+    });
+
+  function displayResult(inputId, fetchUrl) {
     const resultContainer = document.querySelector(".result-container");
     resultContainer.style.display = "block";
     resultContainer.classList.add("animate-result");
 
     const target = document.getElementById("sentiment-gauge");
-    const gauge = new Gauge(target).setOptions(opts);
+    const gauge = createGauge(target, opts);
+    const sentimentResult = document.getElementById("sentiment-result");
+    const url = document.getElementById(inputId).value;
+
+    if (fetchUrl) {
+      fetchData(fetchUrl, { url }).then((data) =>
+        updateUI(data, gauge, sentimentResult)
+      );
+    } else {
+      const article = article_list.find(
+        (article) => article.article_url === url
+      );
+      updateUI(article, gauge, sentimentResult);
+      document.getElementById("val-url").value = url;
+    }
+  }
+
+  function createGauge(target, options) {
+    const gauge = new Gauge(target).setOptions(options);
     gauge.maxValue = 1000;
     gauge.setMinValue(0);
     gauge.animationSpeed = 32;
     gauge.set(0);
+    return gauge;
+  }
 
-    const sentimentResult = document.getElementById("sentiment-result");
-    const url = document.getElementById("val-url").value;
-    fetch("/analyze", {
+  function fetchData(url, body) {
+    return fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ url }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        data.score = data.score < 0 ? data.score * -1 : data.score;
+      body: JSON.stringify(body),
+    }).then((response) => response.json());
+  }
 
-        gauge.set(data.score * 1000);
-        const percentage = (data.score * 100).toFixed(2);
-        sentimentResult.innerHTML = `${percentage}% is ${data.sentiment}`;
-
-        document.getElementById(
-          "word-cloud"
-        ).src = `data:image/png;base64,${data.wordcloud}`;
-      });
-  });
-  document
-    .getElementById("article-list")
-    .addEventListener("change", function () {
-      // Get info from the selected article and populate the form using the global article_list
-      // Example data: 
-      const selectedArticleUrl = this.value;
-      const article = article_list.find(
-        (article) => article.article_url === selectedArticleUrl
-      );
-
-      document.getElementById("val-url").value = article.article_url;
-
-      const resultContainer = document.querySelector(".result-container");
-      resultContainer.style.display = "block";
-      resultContainer.classList.add("animate-result");
-
-      const target = document.getElementById("sentiment-gauge");
-      const gauge = new Gauge(target).setOptions(opts);
-      gauge.maxValue = 1000;
-      gauge.setMinValue(0);
-      gauge.animationSpeed = 32;
-      gauge.set(0);
-    });
+  function updateUI(data, gauge, sentimentResult) {
+    data.score = data.score < 0 ? data.score * -1 : data.score;
+    gauge.set(data.score * 1000);
+    const percentage = (data.score * 100).toFixed(2);
+    sentimentResult.innerHTML = `${percentage}% is ${data.sentiment}`;
+    document.getElementById(
+      "word-cloud"
+    ).src = `data:image/png;base64,${data.wordcloud}`;
+  }
 
   function populateArticleList() {
     fetch("/static/analysis.json")
@@ -85,17 +90,22 @@ window.onload = function () {
       .then((data) => {
         article_list = data;
         const articleList = document.getElementById("article-list");
-
         const emptyOption = document.createElement("option");
         emptyOption.value = "";
         emptyOption.textContent = "Select an article...";
         articleList.appendChild(emptyOption);
 
+        const articleUrls = new Set();
+
         for (const article of data) {
-          const option = document.createElement("option");
-          option.value = article.article_url;
-          option.textContent = article.title;
-          articleList.appendChild(option);
+          if (!articleUrls.has(article.article_url)) {
+            const option = document.createElement("option");
+            option.value = article.article_url;
+            option.textContent = article.title;
+            articleList.appendChild(option);
+
+            articleUrls.add(article.article_url);
+          }
         }
       });
   }
